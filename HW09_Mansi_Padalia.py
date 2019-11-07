357a3d21a2d2d5f4c6700886cb6b347a4480140c
Mansi Padalia <mansi25padalia@gmail.com>
2019-11-06 21:30:31 -0500

    completed HW10

--- HW09_Mansi_Padalia.py
+++ HW09_Mansi_Padalia.py
@@ -11,48 +11,87 @@ from prettytable import PrettyTable
 
 
 class Repository():
-    """ stores students, instructors and grades for a university """
+    """ stores students, instructors, grades and majors for a university """
     students = dict()
     instructors = dict()
+    majors = dict()
 
     def __init__(self, directory):
-        """ initializes the directory path for the university and reads the students, instructors and grades files """
+        """ initializes the directory path for the university and reads the students, instructors, grades and majors files """
         self.directory = directory
-        self.get_students()
-        self.get_instructors()
-        self.get_grades()
+        try:
+            self.get_majors()
+            self.get_students()
+            self.get_instructors()
+            self.get_grades()
+        except Exception as err:
+            raise err
 
     def get_students(self):
         """ reads the students file for the university and stored it in repository """
         student_records = read_file_generator(
-            self.directory, 'students.txt', 3)
+            self.directory, 'students.txt', 3, ';', True)
         for cwid, name, major in student_records:
-            self.students[cwid] = Student(cwid, name, major)
+            if major in self.majors.keys():
+                self.students[cwid] = Student(cwid, name, major)
+            else:
+                raise ValueError(
+                    f"{major} is not a recognized major in 'major.txt' file.")
 
     def get_instructors(self):
         """ reads the instructors file for the university and stored it in repository """
         instructor_records = read_file_generator(
-            self.directory, 'instructors.txt', 3)
+            self.directory, 'instructors.txt', 3, '|', True)
         for cwid, name, department in instructor_records:
             self.instructors[cwid] = Instructor(cwid, name, department)
 
     def get_grades(self):
         """ reads the grades file for the university and link it with students and instructors in repository """
-        grade_records = read_file_generator(self.directory, 'grades.txt', 4)
+        grade_records = read_file_generator(
+            self.directory, 'grades.txt', 4, '|', True)
         for student_cwid, course, letter_grade, instructor_cwid in grade_records:
             if student_cwid in self.students.keys():
                 self.students[student_cwid].student_courses = (
                     course, letter_grade)
                 if instructor_cwid in self.instructors.keys():
                     self.instructors[instructor_cwid].instructor_courses = course
+                else:
+                    raise ValueError(
+                        f"{instructor_cwid} is not a recognized instructor in 'instructor.txt' file.")
+            else:
+                raise ValueError(
+                    f"{student_cwid} is not a recognized student in 'students.txt' file.")
+
+    def get_majors(self):
+        """ reads the majors file for the university and stored it in repository """
+        major_records = read_file_generator(
+            self.directory, 'majors.txt', 3, '\t', True)
+        for major, reflag, course in major_records:
+            if major not in self.majors:
+                self.majors[major] = Major(major)
+            if reflag in ('r', 'R'):
+                self.majors[major].required_courses = course
+            elif reflag in ('e', 'E'):
+                self.majors[major].elective_courses = course
+
+    def major_table(self):
+        """ prints out the pretty table for majors """
+        major_table = PrettyTable(
+            field_names=['Dept', 'Required', 'Electives'])
+        for dept, major in self.majors.items():
+            major_table.add_row(
+                [dept, sorted(list(major.required_courses)), sorted(list(major.elective_courses))])
+        return major_table
 
     def student_table(self):
         """ prints out the pretty table for students """
         student_table = PrettyTable(
-            field_names=['CWID', 'Name', 'Completed Courses'])
+            field_names=['CWID', 'Name', 'Major', 'Completed Courses', 'Remaining Required', 'Remaining Electives'])
         for cwid, student in self.students.items():
+            completed_courses, remaining_required, remaining_elective = self.majors[student.major].remaining_courses(
+                student.student_courses)
             student_table.add_row(
-                [cwid, student.name, sorted(list(student.student_courses))])
+                [cwid, student.name, student.major, sorted(completed_courses), remaining_required, remaining_elective])
         return student_table
 
     def instructor_table(self):
@@ -79,7 +118,7 @@ class Student:
     @property
     def student_courses(self):
         """ experimenting with @property annotation to return all course names for a student """
-        return self._student_courses.keys()
+        return self._student_courses
 
     @student_courses.setter
     def student_courses(self, course_grade):
@@ -123,6 +162,56 @@ class Instructor:
         return "<instructor: {} {}>".format(self.cwid, self.name)
 
 
+class Major:
+    """ a class to store majors related information"""
+
+    def __init__(self, major):
+        """ stores instructor's basic information while creation of an object """
+        self.major = major
+        self._required_courses = set()
+        self._elective_courses = set()
+
+    @property
+    def required_courses(self):
+        """ experimenting with @property annotation to return set of required courses for a major """
+        return self._required_courses
+
+    @required_courses.setter
+    def required_courses(self, course):
+        """ experimenting with @attribute.setter annotation to add course to required courses for a major """
+        self._required_courses.add(course)
+
+    @property
+    def elective_courses(self):
+        """ experimenting with @property annotation to return set of elective courses for a major """
+        return self._elective_courses
+
+    @elective_courses.setter
+    def elective_courses(self, course):
+        """ experimenting with @attribute.setter annotation to add course to elective courses for a major """
+        self._elective_courses.add(course)
+
+    def remaining_courses(self, student_courses):
+        """ return a tuple of completed courses, remaining required courses and remaining elective courses for a student with a given major"""
+        student_required = self.required_courses.copy()
+        student_elective = self.elective_courses.copy()
+        student_completed = list(student_courses.keys()).copy()
+        passing_grades = ['A', 'A-', 'B+', 'B', 'B-', 'C+', 'C']
+        for course, grade in student_courses.items():
+            if grade in passing_grades:
+                if course in student_required:
+                    student_required.remove(course)
+                elif course in student_elective:
+                    student_elective = set()
+            else:
+                student_completed.remove(course)
+        return student_completed, student_required if student_required else None, student_elective if student_elective else None
+
+    def __str__(self):
+        """ returns major with its required and elective courses as a string """
+        return "<major: {}, required courses: {}, elective courses: {}>".format(self.major, ", ".join(list(self.required_courses)), ", ".join(list(self.elective_courses)))
+
+
 def read_file_generator(directory, filename, fields, sep='\t', header=False):
     """ a generator function to read field-separated text files and yield a tuple with all of the values from a single line in the file """
     path = os.path.join(directory, filename)
@@ -141,7 +230,7 @@ def read_file_generator(directory, filename, fields, sep='\t', header=False):
                 # if number of fields expected is not equal to the one generated by spliting separator then raise error
                 if len(words_list) != fields:
                     raise ValueError(
-                        f"{path} has {len(words_list)} on line {line_number} but expected {fields}")
+                        f"{path} has {len(words_list)} fields on line {line_number} but expected {fields}")
                 # if there is a header line then skip it and don't yield
                 if(header and line_number == 1):
                     continue
@@ -150,12 +239,17 @@ def read_file_generator(directory, filename, fields, sep='\t', header=False):
 
 def main():
     """ framework for the project to summarize student and instructor data """
-    stevens_directory = '/Volumes/Macintosh HD/Users/pratik/Courses/SSW810A/Week9/Stevens'
-    stevens_repository = Repository(stevens_directory)
-    print("Student Summary")
-    print(stevens_repository.student_table())
-    print("Instructor Summary")
-    print(stevens_repository.instructor_table())
+    stevens_directory = '/Volumes/Macintosh HD/Users/pratik/Courses/SSW810A/PythonProject/Stevens'
+    try:
+        stevens_repository = Repository(stevens_directory)
+        print("Majors Summary")
+        print(stevens_repository.major_table())
+        print("Student Summary")
+        print(stevens_repository.student_table())
+        print("Instructor Summary")
+        print(stevens_repository.instructor_table())
+    except Exception as err:
+        print(f"{err.__class__.__name__}: {err}")
 
 
 if __name__ == "__main__":
--- HW09_Test_Mansi_Padalia.py
+++ HW09_Test_Mansi_Padalia.py
@@ -7,7 +7,7 @@ Date: 10/23/2019
 
 import unittest
 from prettytable import PrettyTable
-from HW09_Mansi_Padalia import Repository, Student, Instructor, read_file_generator
+from HW09_Mansi_Padalia import Repository, Student, Instructor, Major, read_file_generator
 
 
 class TestRpository(unittest.TestCase):
@@ -15,7 +15,7 @@ class TestRpository(unittest.TestCase):
 
     def test_read_file_generator(self):
         """ verify the generator function to read field-separated text files and yield a tuple with all of the values from a single line in the file """
-        directory = '/Volumes/Macintosh HD/Users/pratik/Courses/SSW810A/Week9'
+        directory = '/Volumes/Macintosh HD/Users/pratik/Courses/SSW810A/PythonProject'
         filename = 'HW09_ReadFile.txt'
         gen = read_file_generator(directory, filename, 3, sep='|', header=True)
         expected_result = [('123', 'Jin He', 'Computer Science'), ('234', 'Nanda Koka',
@@ -40,23 +40,49 @@ class TestRpository(unittest.TestCase):
             next(gen)
 
     def test_create_repository(self):
-        """ verify that given a directory, it creates repository instance with all students and instructor information """
-        test_directory = '/Volumes/Macintosh HD/Users/pratik/Courses/SSW810A/Week9/Test'
+        """ verify that given a directory, it creates repository instance with all majors, students and instructor information """
+        test_directory = '/Volumes/Macintosh HD/Users/pratik/Courses/SSW810A/PythonProject/Test'
         test_repository = Repository(test_directory)
 
+        expected_majors_table = PrettyTable(
+            field_names=['Dept', 'Required', 'Electives'])
+        expected_majors_table.add_row(
+            ['CS', sorted(['CS 510', 'CS 520']), sorted(['SSW 500', 'SSW 510', 'SSW 520', 'SSW 530', 'SSW 540'])])
+        expected_majors_table.add_row(
+            ['SSW', sorted(['SSW 500', 'SSW 510', 'SSW 520', 'SSW 530', 'SSW 540']), sorted(['CS 510', 'CS 520'])])
+        self.assertEqual(expected_majors_table.get_string(),
+                         test_repository.major_table().get_string())
+
         expected_student_table = PrettyTable(
-            field_names=['CWID', 'Name', 'Completed Courses'])
+            field_names=['CWID', 'Name', 'Major', 'Completed Courses', 'Remaining Required', 'Remaining Electives'])
         expected_student_table.add_row(
-            [1, 'Baldwin, C', sorted(['SSW 500', 'SSW 510', 'CS 520'])])
+            [1, 'Baldwin, C', 'CS', sorted(['CS 520', 'SSW 500', 'SSW 510']), {'CS 510'}, None])
         expected_student_table.add_row(
-            [2, 'Wyatt, X', sorted(['SSW 500', 'CS 510', 'SSW 520'])])
-        expected_student_table.add_row([3, 'Forbes, I', sorted(
-            ['SSW 500', 'SSW 510', 'CS 510', 'SSW 520', 'SSW 530'])])
+            [2, 'Wyatt, X', 'SSW', sorted(['CS 510', 'SSW 500']), {'SSW 510', 'SSW 520', 'SSW 530', 'SSW 540'}, None])
+        expected_student_table.add_row([3, 'Forbes, I', 'CS', sorted(
+            ['CS 510', 'SSW 500', 'SSW 510', 'SSW 520', 'SSW 530']), {'CS 520'}, None])
         expected_student_table.add_row(
-            [4, 'Erickson, D', sorted(['SSW 520', 'SSW 540'])])
-        expected_student_table.add_row([5, 'Chapman, O', sorted([])])
-        self.assertEqual(expected_student_table.get_string(),
-                         test_repository.student_table().get_string())
+            [4, 'Erickson, D', 'SSW', sorted(['SSW 520', 'SSW 540']), {'SSW 500', 'SSW 510', 'SSW 530'}, {'CS 510', 'CS 520'}])
+        expected_student_table.add_row([5, 'Chapman, O', 'CS', sorted(
+            []), {'CS 510', 'CS 520'}, {'SSW 500', 'SSW 510', 'SSW 520', 'SSW 530', 'SSW 540'}])
+
+        for expected_row, test_repository_row in zip(expected_student_table, test_repository.student_table()):
+            expected_row.border = False
+            expected_row.header = False
+            test_repository_row.border = False
+            test_repository_row.header = False
+            self.assertEqual(expected_row.get_string(fields=["CWID"]).strip(),
+                             test_repository_row.get_string(fields=["CWID"]).strip())
+            self.assertEqual(expected_row.get_string(fields=["Name"]).strip(),
+                             test_repository_row.get_string(fields=["Name"]).strip())
+            self.assertEqual(expected_row.get_string(fields=["Major"]).strip(),
+                             test_repository_row.get_string(fields=["Major"]).strip())
+            self.assertEqual(expected_row.get_string(fields=["Completed Courses"]).strip(),
+                             test_repository_row.get_string(fields=["Completed Courses"]).strip())
+            self.assertEqual(set(expected_row.get_string(fields=["Remaining Required"]).strip()),
+                             set(test_repository_row.get_string(fields=["Remaining Required"]).strip()))
+            self.assertEqual(set(expected_row.get_string(fields=["Remaining Electives"]).strip()),
+                             set(test_repository_row.get_string(fields=["Remaining Electives"]).strip()))
 
         expected_instructor_table = PrettyTable(
             field_names=['CWID', 'Name', 'Dept', 'Course', 'Students'])
